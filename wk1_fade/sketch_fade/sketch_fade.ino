2ec880faada634e19148d3643ba76c073935a53f
Emily Lin <el3015@nyu.edu>
2019-02-03 23:11:40 -0500

    added 5 led pins

--- wk1_fade/sketch_fade/sketch_fade.ino
+++ wk1_fade/sketch_fade/sketch_fade.ino
@@ -2,33 +2,48 @@ int intensity = 255;
 float change = 2.55;
 int pot_pin = A0;
 int no_intensity = 0;
+int led_1 = 3;
+int led_2 = 5;
+int led_3 = 6;
+int led_4 = 10;
+int led_5 = 11;
+
+int ledPins[] = {3,5,6,10,11};
 
 int start;
 
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(5, OUTPUT);
+  Serial.begin(9600);
+  
+  for (int i = 0; i < 5; i++){
+   pinMode(ledPins[i], OUTPUT);
+  }
   start = true;
 }
 
 void loop() {
   // read if pot has been turned on
-  start = (analogRead(pot_pin) > 0);
+  start = (analogRead(pot_pin));
+  Serial.println(start);
   
   // if the pot is turned on, fade in and out
   if (start){
-      if (intensity <= 0 || intensity>= 255){
+      if (intensity <= 0 || intensity >= 255){
       change = -change;
   }
   intensity += change;
-  analogWrite(5, intensity);
+  for (int i = 0; i<5; i++){
+    analogWrite(ledPins[i], intensity);
+  }
   delay(50);
   }
 
   else {
     start = false;
-    analogWrite(5, no_intensity);
+    for (int i = 0; i < 5; i++) {
+      analogWrite(ledPins[i], no_intensity);
+    }
   }
 }
 
