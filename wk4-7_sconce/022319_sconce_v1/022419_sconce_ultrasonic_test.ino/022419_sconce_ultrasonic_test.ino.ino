9d1e85c419d05468f88ba72ecbbb313a1e1c158c
Emily Lin <el3015@nyu.edu>
2019-02-25 10:11:03 -0500

    testing ultrasonic

---
+++ wk4-7_sconce/022319_sconce_v1/022419_sconce_ultrasonic_test.ino/022419_sconce_ultrasonic_test.ino.ino
@@ -0,0 +1,32 @@
+/* Ultrasonic Code not my own!
+    Code from tutorial:
+    https://www.youtube.com/watch?v=ZejQOX69K5M
+*/
+
+const int trigPin = 12;
+const int echoPin = 11;
+
+long duration;
+int distance;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // high means pulsing function will wait for the pin to go high caused by the bounce soundwave
+  duration = pulseIn (echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+}
