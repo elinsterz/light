f7ddd4e177b7f86e28dc63ee4362bae9157f3b71
Emily Lin <el3015@nyu.edu>
2019-04-14 00:21:20 -0400

    updates

--- wk8_dmx/controller/server.js
+++ wk8_dmx/controller/server.js
@@ -1,12 +1,18 @@
 var http = require('http');
-var module1 = require('./module1'); //same dir use ./
-var module2 = require('./module2'); //same dir use ./
+var fs = require('fs'); //get file system
 
 function onRequest(request, response){
-    response.writeHead(200,{'Content-Type':'text/plain'});
-    response.write('Hello');
-    module2.myFunction();
-    response.end();
+    response.writeHead(200,{'Content-Type':'text/html'});
+    fs.readFile('./index.html', null, function(error, data){
+        if(error){
+            response.writeHead(404);
+            response.write('File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+        }
+    });
 }
 
 http.createServer(onRequest).listen(8000);  //creates server and listen to port 8000
