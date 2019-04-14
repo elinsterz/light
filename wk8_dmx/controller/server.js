3fa03c428c65853e1c949b27a8e66651d33e563f
Emily Lin <el3015@nyu.edu>
2019-04-14 00:12:56 -0400

    follow along tutorial code
    following along with this person's tutorial:https://www.youtube.com/watch?v=ioxgbkxIGwE&list=PL55RiY5tL51oGJorjEgl6NVeDbx_fO5jR&index=4

--- wk8_dmx/controller/server.js
+++ wk8_dmx/controller/server.js
@@ -1,10 +1,11 @@
 var http = require('http');
 var module1 = require('./module1'); //same dir use ./
+var module2 = require('./module2'); //same dir use ./
 
 function onRequest(request, response){
     response.writeHead(200,{'Content-Type':'text/plain'});
-    response.write(module1.myString);
-    module1.myFunction();
+    response.write('Hello');
+    module2.myFunction();
     response.end();
 }
 
