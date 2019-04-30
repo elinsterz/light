cd8802a813848c67e69ba1331394850d5f3bf72c
Emily Lin <el3015@nyu.edu>
2019-04-29 22:03:20 -0400

    update

---
+++ wk10-14_CO2/CCS811_test/CCS811_test.ino
@@ -0,0 +1,68 @@
+/***************************************************************************
+  This is a library for the CCS811 air
+
+  This sketch reads the sensor
+
+  Designed specifically to work with the Adafruit CCS811 breakout
+  ----> http://www.adafruit.com/products/3566
+
+  These sensors use I2C to communicate. The device's I2C address is 0x5A
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Dean Miller for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+#include "Adafruit_CCS811.h"
+
+
+Adafruit_CCS811 ccs;
+
+bool dataAvailable = ccs.available(); //returns true if data is available to be read
+bool error = ccs.readData(); //returns True if an error occurs during the read
+
+int eCO2 = ccs.geteCO2(); //returns eCO2 reading
+int TVOC = ccs.getTVOC(); //return TVOC reading
+
+float temp = ccs.calculateTemperature();
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+
+  }
+
+  Serial.println("CCS811 test");
+
+  if (!ccs.begin()) {
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while (1);
+  }
+
+  //calibrate temperature sensor
+  while (!ccs.available());
+  float temp = ccs.calculateTemperature();
+  ccs.setTempOffset(temp - 25.0);
+}
+
+void loop() {
+  if (ccs.available()) {
+    float temp = ccs.calculateTemperature();
+    if (!ccs.readData()) {
+      Serial.print("CO2: ");
+      Serial.print(ccs.geteCO2());
+      Serial.print("ppm, TVOC: ");
+      Serial.print(ccs.getTVOC());
+      Serial.print("ppb   Temp:");
+      Serial.println(temp);
+    }
+    else {
+      Serial.println("ERROR!");
+      while (1);
+    }
+  }
+  delay(500);
+}
